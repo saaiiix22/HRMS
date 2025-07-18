8c60bbf9b6eede4427c9ab96852edc29e8c427fc
saaiiix22 <sai.naivedya123@gmail.com>
2025-07-18 15:15:31 +0530

    db url addition

--- backend/db_connection/dbConnection.js
+++ backend/db_connection/dbConnection.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const dbConnection = async()=>{
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/HRMS' || process.env.DBURL)
+        // const conn = await mongoose.connect('mongodb://localhost:27017/HRMS' || process.env.DBURL)
+        const conn = await mongoose.connect('mongodb+srv://aashditHRMS:3myuODvOmYcEjdy5@cluster0.lg1lsh4.mongodb.net/HRMS' || process.env.DBURL)
+
         conn?console.log('DB Connected'):console.log('DB Connection Failed');
     } catch (error) {
         console.log(error);
